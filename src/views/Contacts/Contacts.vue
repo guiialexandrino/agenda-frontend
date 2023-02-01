9827f12de9b9b83c3535d9bd7d4e5cb09d9fd15b
Guilherme <guiialexandrino@gmail.com>
2023-02-01 13:23:44 -0300

    Pagina Contacts andamento

--- src/views/Contacts/Contacts.vue
+++ src/views/Contacts/Contacts.vue
@@ -1,24 +1,17 @@
 <template>
-  <header>
-    <div class="maxContent header-nav">Header aqui</div>
-  </header>
-  <content>
-    <div class="maxContent wrapper-content">
-      <div class="content-top">
-        <div class="photo">Photo</div>
-        <div class="info">
-          <div class="info-label">
-            <h1>Aristeu Arissimba</h1>
-            <h2>Você tem 9 contatos.</h2>
-          </div>
-        </div>
+  <section>
+    <div class="wrapper-content">
+      <div class="contacts">
+        <h1>teste</h1>
+        <h1>teste</h1>
+        <h1>teste</h1>
       </div>
-      <div><h1>table</h1></div>
+      <div class="container-photo">
+        <div class="background-color"></div>
+        <div class="photo"></div>
+      </div>
     </div>
-  </content>
-  <footer>
-    <div class="maxContent footer-nav">Footer</div>
-  </footer>
+  </section>
 </template>
 
 <script setup></script>
