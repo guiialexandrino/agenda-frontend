f20addb69c12689f1889dab365769e3a3beac170
Guilherme <guiialexandrino@gmail.com>
2023-02-15 20:35:25 -0300

    Fixes para buildar em netlify

--- src/requisitions/base/baseUrl.js
+++ src/requisitions/base/baseUrl.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
+/*For local and https */
+
 export const url = 'http://localhost:80';
 export const upload = `${url}/uploads`;
-
 const baseUrl = axios.create({ baseURL: url });
+
+/* For Netlify Build -> Comment the lines above! */
+
+// export const upload = `connect/uploads`;
+// const baseUrl = axios.create({ baseURL: "connect" });
 
 export default baseUrl;
